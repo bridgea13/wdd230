dd56b746e78f9db5c16592395138f066e8820379
bridgea13 <bri21025@byui.edu>
2023-05-30 22:42:33 -0400

    fixed errors from feedback, opt img, js only in js not html, etc

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the current year
-    var currentYear = new Date().getFullYear();
-    
-    // Update the footer with the current year
-    var yearElement = document.getElementById("year");
-    yearElement.textContent = currentYear;
-  });
 
-  var currentYear = new Date().getFullYear();
-  
-    var currentYearElement = document.querySelector("#currentYear");
-    currentYearElement.textContent = currentYear;
-
-
-// const footerText = document.getElementById('copyright');
-// const currentYear = new Date().getFullYear();
-// const lastUpdated = new Date().toLocaleDateString();
-// footerText.textContent = `© ${currentYear} My
+document.querySelector(".yr").textContent = (new Date().getFullYear());
