6f7f6544d6211fdb1f5069ecc77b05aead75dc56
bridgea13 <bri21025@byui.edu>
2023-06-21 20:15:20 -0400

    errors fixed in assignment 1

--- scripts/script.js
+++ scripts/script.js
@@ -8,4 +8,3 @@ var yearElement = document.getElementById("year");
 yearElement.textContent = date.getFullYear();
   
 document.querySelector("#lastModified").textContent=document.lastModified;
-document.querySelector(".current-date").textContent=date.toLocaleDateString("en-GB",{ weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' });
